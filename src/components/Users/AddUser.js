5eada36750ed42d849ffa748c1dd2693e5d87633
xXnielsXx <nielsmichael@comcast.net>
2022-05-09 22:32:00 +0900

    Implement ErrorModal conditional rendering

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -8,15 +8,27 @@ const AddUser = (props) => {
   // Store the entered user with useState
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  // useState for error handling
+  const [error, setError] = useState();
 
   const addUserHandler = (e) => {
     e.preventDefault(); // prevent page reload
     // Check if both inputs are blank
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      // Set error state to an object
+      setError({
+        title: "Invalid Input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
       return;
     }
     // Check if age is invalid
     if (+enteredAge < 1) {
+      // Set error state
+      setError({
+        title: "Invalid Number",
+        message: "Please enter a valid number (greater than 0).",
+      });
       // + sign ensures js evaluates enteredAge as a number and NOT a string
       return;
     }
@@ -40,26 +52,42 @@ const AddUser = (props) => {
     setEnteredAge(e.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={enteredUsername}
-          onChange={usernameChangeHandler}
-        ></input>
-        <label htmlFor="age">Age (Years)</label>
-        <input
-          id="age"
-          type="number"
-          value={enteredAge}
-          onChange={ageChangeHandler}
-        ></input>
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+    <div>
+      {
+        // Set ErrorModal to render on the condition that error contains any sort of state
+        error && (
+          <ErrorModal
+            title={error.title}
+            message={error.message}
+            onConfirm={errorHandler}
+          />
+        )
+      }
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          ></input>
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          ></input>
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
